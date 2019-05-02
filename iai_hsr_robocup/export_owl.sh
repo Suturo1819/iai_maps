8357775f964e59c63c445302959766b924f02acb
Arthur Niedzwiecki <arthur.niedzwiecki@gmail.com>
2019-05-03 00:25:07 +0200

    Set living room as default. Add OWL exporter scritp

---
+++ iai_hsr_robocup/export_owl.sh
@@ -0,0 +1,1 @@
+rosrun xacro xacro urdf/HSR_robocup.urdf.xacro > urdf/HSR_robocup.urdf && rosrun knowrob_srdl URDF2SRDL -u urdf/HSR_robocup.urdf -s owl/HSR_robocup.owl -i http://knowrob.org/kb/robocup.owl#
